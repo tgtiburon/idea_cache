7340624ac7f15519d282a47346a66866fd06eff2
tgtiburon <tg.tiburon@gmail.com>
2021-12-09 15:48:20 -0500

    Total restructure of files.  I was using a develop file which made heroku die

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,9 +3,11 @@ const path = require('path');
 //load express's router
 const router = require('express').Router();
 
+console.log("in html index dirname = ", __dirname);
+
 // If user sends / send index
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
   });
 
 // If user sends /notes send notes.html
